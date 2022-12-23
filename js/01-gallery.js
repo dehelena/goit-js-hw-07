1db927e4372d4e7ca662d06c0fe6e4ac62fd103e
Olena Denysenko <helenazinchenko@ukr.net>
2022-12-23 20:13:30 +0200

    ready markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+
+// console.log(createGalleryItem(galleryItems));
+
+function createGalleryItem(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href='${original}'>
+      <img
+        class="gallery__image"
+        src='${preview}'
+        data-source="${original}"
+        alt='${description}'
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+// galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+// function onGalleryContainerClick(event) {
+//   if (event.target.nodeName !== "img") {
+//     return;
+//   }
+
+//   console.log(event.target.textContent);
+// }
+
+// console.log(galleryItems);
