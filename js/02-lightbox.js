dd54a7edf3e727849c64b1939476905f561f5a07
Olena Denysenko <helenazinchenko@ukr.net>
2022-12-26 00:14:16 +0200

    Task-02 begin

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const sourceUrl = e.target.dataset.source;
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    /* options */
+  });
+}
+
+// console.log(galleryItems);
